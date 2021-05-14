2c41421ff26be58c335976316918fbe336324654
VishnuPillai135 <84180136+VishnuPillai135@users.noreply.github.com>
2021-05-14 20:45:34 +1000

    Create testing.c
    More advanced testing that runs the program using the bot.

---
+++ testing.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main (void) {
+
+
+    x = 
+    if (round == 1) {
+        if (position == 1) {
+            scanf("%d", &used_card_game[0]);
+        }
+        if (position == 2) {
+            scanf("%d", &used_card_game[0]);
+            scanf("%d", &used_card_game[1]);
+        }
+        if (position == 3) {
+            scanf("%d", &used_card_game[0]);
+            scanf("%d", &used_card_game[1]);
+            scanf("%d", &used_card_game[2]);
+        }
+    }
+    if (round == 2) {  
+        if (position == 1) {
+            scanf("%d", &
+            
+            
+    int x = 0 + ((round - 1) * 4);
+    int y = 1 + ((round - 1) * 4);
+    int z = 2 + ((round - 1) * 4); 
+    int c = 3 + ((round - 1) * 4);  
+            
+            
+    if (position == 0) {
+        while (i <= x) {
+            scanf("%d" &used_card_game[i]);
+        }
+    }
+    if (position == 1) {
+        while (i <= y) {
+            scanf("%d" &used_card_game[i]);
+        }
+    }
+    if (position == 2) {
+        while (i <= z) {
+            scanf("%d" &used_card_game[i]);
+        }
+    }
+    if (position == 3) {
+        while (i <= c) {
+            scanf("%d" &used_card_game[i]);
+        }
+    }
+        
